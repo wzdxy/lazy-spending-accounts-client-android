c5d6503fd4cd203a6f68f236dd5e5013cd01cee8
zc <zc_93@qq.com>
2017-12-19 00:38:12 +0800

    页面导航 , 滑动切换

--- app/src/main/java/me/zchi/lazyaccounts/MainActivity.java
+++ app/src/main/java/me/zchi/lazyaccounts/MainActivity.java
@@ -1,12 +1,15 @@
 package me.zchi.lazyaccounts;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,25 +18,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private BottomNavigationView navigation;
+    private ViewPager viewPager;
+
+    private AddFragment addFragment = new AddFragment();
+    private AccountFragment accountFragment = new AccountFragment();
+    private ReportFragment reportFragment = new ReportFragment();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_account:
-                    mTextMessage.setText(R.string.title_account);
-                    return true;
-                case R.id.navigation_report:
-                    mTextMessage.setText(R.string.title_report);
-                    return true;
-            }
-            return false;
+            viewPager.setCurrentItem(item.getOrder());
+            return true;
         }
     };
 
@@ -56,23 +54,75 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return false;
-    };
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main,menu);
         return true;
-    };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+        navigation = findViewById(R.id.navigation);
+        viewPager = findViewById(R.id.viewPager);
+
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return addFragment;
+                    case 1:
+                        return accountFragment;
+                    case 2:
+                        return reportFragment;
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.e("PageSelected", String.valueOf(position));
+                switch (position) {
+                    case 0:
+                        navigation.setSelectedItemId(R.id.navigation_home);
+                        break;
+                    case 1:
+                        navigation.setSelectedItemId(R.id.navigation_account);
+                        break;
+                    case 2:
+                        navigation.setSelectedItemId(R.id.navigation_report);
+                        break;
+                }
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+
+
 
     }
 
